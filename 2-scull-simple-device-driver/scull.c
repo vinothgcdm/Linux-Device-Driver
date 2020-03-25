e7d19eef3a6d6a112d3c51f7453dd70e4c0ecdb0
vinothgcdm <vinothg.cdm@gmail.com>
2020-03-25 21:40:34 +0800

    Remove scull.h from all 2a-project and its sub project.

--- 2-scull-simple-device-driver/scull.c
+++ 2-scull-simple-device-driver/scull.c
@@ -2,12 +2,35 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include "scull.h"
 
 static int scull_major = 0;
 static int scull_minor = 0;
 static int scull_nr_devs = 1;
 static struct cdev cdev;
+
+static int scull_open(struct inode *inode, struct file *filp)
+{
+    printk("scull_open\n");
+    return 0;
+}
+
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+{
+    printk("scull_read\n");
+    return 0;
+}
+
+static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+{
+    printk("scull_write\n");
+    return count;
+}
+
+static int scull_release(struct inode *inode, struct file *filp)
+{
+    printk("scull_release\n");
+    return 0;
+}
 
 static struct file_operations scull_fops = {
     .owner = THIS_MODULE,
@@ -19,62 +42,38 @@ static struct file_operations scull_fops = {
 
 static int scull_init(void)
 {
-	dev_t dev = 0;
-	int ret = 0;
+    dev_t dev = 0;
+    int ret = 0;
 
-	printk("Hello from Scull\n");
-	ret = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
-	if (ret < 0) {
-		printk("Allocating device number is failed.\n");
-		return ret;
-	}
+    printk("Hello from Scull\n");
+    ret = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
+    if (ret < 0) {
+        printk("Allocating device number is failed.\n");
+        return ret;
+    }
 
-	scull_major = MAJOR(dev);
-	scull_minor = MINOR(dev);
-	printk("Major: %d, Minor: %d\n", scull_major, scull_minor);
+    scull_major = MAJOR(dev);
+    scull_minor = MINOR(dev);
+    printk("Major: %d, Minor: %d\n", scull_major, scull_minor);
 
-	cdev_init(&cdev, &scull_fops);
-	ret = cdev_add(&cdev, dev, 1);
-	if (ret < 0) {
-		unregister_chrdev_region(dev, scull_nr_devs);
-		printk("Add scull device into kernel failed.\n");
-		return ret;
-	}
+    cdev_init(&cdev, &scull_fops);
+    ret = cdev_add(&cdev, dev, 1);
+    if (ret < 0) {
+        unregister_chrdev_region(dev, scull_nr_devs);
+        printk("Add scull device into kernel failed.\n");
+        return ret;
+    }
 
-	return 0;
+    return 0;
 }
 
 static void scull_exit(void)
 {
-	dev_t dev = MKDEV(scull_major, scull_minor);
+    dev_t dev = MKDEV(scull_major, scull_minor);
 
-	cdev_del(&cdev);
-	unregister_chrdev_region(dev, scull_nr_devs);
-	printk("Goodbye from Scull\n");
-}
-
-int scull_open(struct inode *inode, struct file *filp)
-{
-	printk("scull_open\n");
-	return 0;
-}
-
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
-{
-	printk("scull_read\n");
-	return 0;
-}
-
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
-{
-	printk("scull_write\n");
-	return count;
-}
-
-int scull_release(struct inode *inode, struct file *filp)
-{
-	printk("scull_release\n");
-	return 0;
+    cdev_del(&cdev);
+    unregister_chrdev_region(dev, scull_nr_devs);
+    printk("Goodbye from Scull\n");
 }
 
 MODULE_LICENSE("Dual BSD/GPL");
--- 2b-completions/scull.c
+++ 2b-completions/scull.c
@@ -25,83 +25,83 @@ DECLARE_COMPLETION(comp);
 
 static int scull_init(void)
 {
-	dev_t dev = 0;
-	int ret = 0;
+    dev_t dev = 0;
+    int ret = 0;
 
-	printk("Hello from Scull\n");
-	ret = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
-	if (ret < 0) {
-		printk("Allocating device number is failed.\n");
-		return ret;
-	}
+    printk("Hello from Scull\n");
+    ret = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs, "scull");
+    if (ret < 0) {
+        printk("Allocating device number is failed.\n");
+        return ret;
+    }
 
-	scull_major = MAJOR(dev);
-	scull_minor = MINOR(dev);
-	printk("Major: %d, Minor: %d\n", scull_major, scull_minor);
+    scull_major = MAJOR(dev);
+    scull_minor = MINOR(dev);
+    printk("Major: %d, Minor: %d\n", scull_major, scull_minor);
 
-	cdev_init(&cdev, &scull_fops);
-	ret = cdev_add(&cdev, dev, 1);
-	if (ret < 0) {
-		unregister_chrdev_region(dev, scull_nr_devs);
-		printk("Add scull device into kernel failed.\n");
-		return ret;
-	}
+    cdev_init(&cdev, &scull_fops);
+    ret = cdev_add(&cdev, dev, 1);
+    if (ret < 0) {
+        unregister_chrdev_region(dev, scull_nr_devs);
+        printk("Add scull device into kernel failed.\n");
+        return ret;
+    }
 
-	return 0;
+    return 0;
 }
 
 static void scull_exit(void)
 {
-	dev_t dev = MKDEV(scull_major, scull_minor);
+    dev_t dev = MKDEV(scull_major, scull_minor);
 
-	cdev_del(&cdev);
-	unregister_chrdev_region(dev, scull_nr_devs);
-	printk("Goodbye from Scull\n");
+    cdev_del(&cdev);
+    unregister_chrdev_region(dev, scull_nr_devs);
+    printk("Goodbye from Scull\n");
 }
 
-int scull_open(struct inode *inode, struct file *filp)
+static int scull_open(struct inode *inode, struct file *filp)
 {
-	printk("scull_open\n");
-	return 0;
+    printk("scull_open\n");
+    return 0;
 }
 
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-	int len = (count > sizeof(kbuf)) ? sizeof(kbuf) : count;
+    int len = (count > sizeof(kbuf)) ? sizeof(kbuf) : count;
 
-	if (sizeof(kbuf) <= *f_pos)
-		return 0;
+    if (sizeof(kbuf) <= *f_pos)
+        return 0;
 
-	printk("scull_read: process %i (%s) going to sleep.\n",
-		current->pid, current->comm);
-	wait_for_completion(&comp);
-	printk("scull_read: awaken %i (%s).\n", current->pid, current->comm);
-	if (printk_ratelimit()) {
-		printk("scull_read: f_pos: %lu, count: %lu\n", *((long*)f_pos), count);
-	}
+    printk("scull_read: process %i (%s) going to sleep.\n",
+        current->pid, current->comm);
+    wait_for_completion(&comp);
+    printk("scull_read: awaken %i (%s).\n", current->pid, current->comm);
+    if (printk_ratelimit()) {
+        printk("scull_read: f_pos: %lu, count: %lu\n", *((long*)f_pos), count);
+    }
 
-	copy_to_user(buf, kbuf, len);
-	*f_pos = len;
+    copy_to_user(buf, kbuf, len);
+    *f_pos = len;
 
-	return len;
+    return len;
 }
 
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
+static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
-	int len = (count > sizeof(kbuf)) ? sizeof(kbuf) : count;
+    int len = (count > sizeof(kbuf)) ? sizeof(kbuf) : count;
 
-	printk("scull_write: process %i (%s) awaking the readers...\n",
-		current->pid, current->comm);
-	copy_from_user(kbuf, buf, len);
-	//complete(&comp);
-	complete_all(&comp);
-	return len;
+    printk("scull_write: process %i (%s) awaking the readers...\n",
+        current->pid, current->comm);
+    copy_from_user(kbuf, buf, len);
+    //complete(&comp);
+    complete_all(&comp);
+    return len;
 }
 
-int scull_release(struct inode *inode, struct file *filp)
+static int scull_release(struct inode *inode, struct file *filp)
 {
-	printk("scull_release\n");
-	return 0;
+    printk("scull_release\n");
+    return 0;
 }
 
 MODULE_LICENSE("Dual BSD/GPL");
